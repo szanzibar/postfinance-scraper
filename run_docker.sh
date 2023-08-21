676400d14fdf09a7581c7d51e35c1b60cc6812fe
szanzibar <email@stevenvandijk.com>
2023-08-21 18:43:16 +0000

    run_docker script stops and removes existing container

--- run_docker.sh
+++ run_docker.sh
@@ -1,3 +1,6 @@
-docker build -t postfinance_scraper . 
+docker build -t postfinance_scraper .
 
-docker run --name postfinance_scraper --net=host -d postfinance_scraper
+docker stop postfinance_scraper
+docker rm postfinance_scraper 2>/dev/null
+
+docker run --name postfinance_scraper --restart unless-stopped --net=host -d postfinance_scraper
