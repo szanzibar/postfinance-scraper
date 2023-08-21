9a31e9d028fea11d1b19234629035d4c3a8bc24b
szanzibar <email@stevenvandijk.com>
2023-08-21 18:48:57 +0000

    ignore stop & rm container errors in run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -1,6 +1,6 @@
 docker build -t postfinance_scraper .
 
-docker stop postfinance_scraper
-docker rm postfinance_scraper 2>/dev/null
+docker stop postfinance_scraper &>/dev/null
+docker rm postfinance_scraper &>/dev/null
 
 docker run --name postfinance_scraper --restart unless-stopped --net=host -d postfinance_scraper
